2e82c24ca98e83591c41dfee8a85989f5f7495b4
Cipher Bentleys <bentleymish05@gmail.com>
2023-12-28 12:26:12 +0300

    Clean data

--- clean.py
+++ clean.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import numpy as np
 
-from Exhibit_A.load import Loader
+from load import Loader
 
 
 # Load the Data for Cleaning
 data_set = Loader.load_data()
 
-print(data_set.columns)
+
+# Dropping Columns
+col_drop = ['Edition Statement', 'Corporate Author', 'Corporate Contributors',
+            'Former owner', 'Engraver', 'Issuance type', 'Flickr URL', 'Shelfmarks']
+
+data_set.drop(columns=col_drop, inplace=True)
+
+
+# Changing Index of a DataFrame
+
+data_set = data_set.set_index('Identifier', inplace=True)
+
+
+# Tidying up Fields in the Data
+
+print(data_set)
