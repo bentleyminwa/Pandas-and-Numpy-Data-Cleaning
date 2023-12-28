73b0b6824c58af2890747a28a38bb83275491880
Cipher Bentleys <bentleymish05@gmail.com>
2023-12-28 11:09:21 +0300

    Load the Dataset

---
+++ load.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Loading Data
+class Loader():
+
+    def __init__(self) -> None:
+        pass
+
+    def load_data():
+        df = pd.read_csv("Data/BL-Flickr-Images-Book.csv")
+
+        return df
