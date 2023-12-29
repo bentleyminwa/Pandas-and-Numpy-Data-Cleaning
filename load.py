ef56fe517962031751316f7d557f9b5d99fd90a6
Cipher Bentleys <bentleymish05@gmail.com>
2023-12-29 11:39:20 +0300

    load .txt file

--- load.py
+++ load.py
@@ -7,6 +7,27 @@ class Loader():
         pass
 
     def load_data():
+        # Loads the book dataset
         df = pd.read_csv("Data/BL-Flickr-Images-Book.csv")
 
         return df
+    
+    def load_town_data():
+        # Loads the university_towns.txt file
+        university_towns = []
+
+        with open('Data/university_towns.txt') as file:
+            for line in file:
+                if '[edit]' in line:
+
+                    # Remember this `state` until the next is found
+                    state = line
+                else:
+
+                    # Otherwise, we have a city; keep `state` as last seen
+                    university_towns.append((state, line))
+
+
+        towns_df = pd.DataFrame(university_towns, columns = ['State', 'RegionName'])
+
+        return towns_df
